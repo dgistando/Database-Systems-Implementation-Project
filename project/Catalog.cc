be61c261d2f9a4e07f8d549e2038c6e05af059f4
Branszin <zgill@ucmerced.edu>
2017-02-15 17:06:12 -0800

    hi
    hi

--- project/Catalog.cc
+++ project/Catalog.cc
@@ -157,6 +157,9 @@ bool Catalog::GetSchema(string& _table, Schema& _schema) {
  */
 bool Catalog::CreateTable(string& _table, vector<string>& _attributes,vector<string>& _attributeTypes) {
     Keyify<string> key(_table);
+    if(_attributes.size() == 0){
+        return false;
+    }
     if(!catalog_tbl.IsThere(key)){
         vector<unsigned int> d;
         for(int i = 0; i < _attributes.size(); i++){ d.push_back(0); }
